e57c4d61af5bd40888d379c80cb93a8f36b4ecab
TodorYanakiev <todoryanakiev395@gmail.com>
2026-01-11 13:29:05 +0200

    fix test issues

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import type { ReactElement } from 'react'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/App.router'
 
+import './locales/i18n'
+
 function App(): ReactElement {
   return <RouterProvider router={router} />
 }
